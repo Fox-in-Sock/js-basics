597f2c4e9d162f2890b43b9f1fa2e1b3b8db95c4
Saksham <camazotz.void@gmail.com>
2025-08-01 19:05:12 +0545

    this is js basics

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-console.log("Welcome to where you should not not be >_< ");
-console.log("hello world");
-document.getElementById("man").innerHTML="hi";
-let name = prompt("What are you and whats your objective: ");
-if (name === "Gag") {
-    let piece_shit = prompt("Are you piece of shit? --> ");
-    if (piece_shit === "Yes") {
-        console.log("Get out");
-        process.exit();
-    }
-} else {
-    console.log("Welcome to the v world now you will forget all about reality " + name + ", Also you cannot return alive.");
-}
-function display(){
-    console.log("hwllo")
-}
+// console.log("Welcome to where you should not not be >_< ");
+// console.log("hello world");
+// document.getElementById("man").innerHTML="hi";
+// let name = prompt("What are you and whats your objective: ");
+// if (name === "Gag") {
+//     let piece_shit = prompt("Are you piece of shit? --> ");
+//     if (piece_shit === "Yes") {
+//         console.log("Get out");
+//         process.exit();
+//     }
+// } else {
+//     console.log("Welcome to the v world now you will forget all about reality " + name + ", Also you cannot return alive.");
+// }
+// function display(){
+//     console.log("hwllo")
+// }
 
-display();
+// display();
+
+
+
+let arr = [1,2,3,4,5];
+document.write(arr)
